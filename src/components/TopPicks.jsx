94af5f81abbffcad6cd602ca7229ee6de80dfd96
Wanderlusst <98679805+Wanderlusst@users.noreply.github.com>
2023-10-11 10:50:30 +0530

    Add files via upload

---
+++ src/components/TopPicks.jsx
@@ -0,0 +1,35 @@
+import { topPicks } from '../data/data';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
+
+const TopPicks = () => {
+  return (
+    <>
+    <div className='text-orange-500 font-bold text-2xl text-center'>TopPicks</div>
+    <div className='hidden lg:flex max-w[1520px] m-auto py-2 px-2'>
+        <Splide options={{perPage:4, gap:"0.5rem",drag:'free',arrows:false}}>
+        {
+            topPicks.map((item,c)=>
+            {
+                return(
+                    <SplideSlide key={c}>
+                    <div  className='rounded-3xl relative'>
+                        <div className='absolute w-full h-full bg-black/50 rounded-3xl text-white'>
+                            <p className='px-2 pt-4'>{item.title}</p>
+                            <p className='px-2'>{item.price}</p>
+                            <button className='border-dotted border-white text-white mx-2 absolute bottom-4 '>Add to cart</button>
+                        </div>
+                        <img className='h-[200px] w-full object-cover rounded-3xl cursor-pointer hover:scale-105 ease-out duration-300' src={item.img}></img>
+                    </div>
+                    </SplideSlide>
+                )
+
+            })
+        }
+        </Splide>
+    </div>
+    </>
+  ) 
+}
+
+export default TopPicks
